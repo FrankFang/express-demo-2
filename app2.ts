7781d013b28f0977f8fa4de093596fab7e62dd1e
frankfang <frankfang1990@gmail.com>
2019-12-17 16:33:38 +0800

    demo

--- app2.ts
+++ app2.ts
@@ -1,9 +1,20 @@
 import express from 'express';
-// 源码里说应该用 import * as express from "express";
-// 但是使用后报错，所以就改了，不用太纠结到底用哪个，哪个能用就用哪个
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use((req, res, next) => {
+  console.log(req.url);
+  res.write('hi'); // 不能用 send 用两次，所以改成 write
+  next();
+});
+app.use((req, res, next) => {
+  console.log(req.url);
+  res.write('hi');
+  next();
+});
+app.use((req, res, next) => {
+  res.end(); // 不 end 不行
+  next(); // next() 好像可以省略
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(3000);
